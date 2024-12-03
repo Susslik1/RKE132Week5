85577ecf62778375bdfb8acbeded39c98c36822d
susslik1 <saskia.suurmets@tktk.ee>
2024-12-03 14:14:18 +0200

    add week 5 code

---
+++ OrOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿//math>= 90 OR Chemisrty>= 90 OR Biology >=90
+
+//math && chemsisty|| math && biology|| chemisrty && biology
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology result:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if((math>= 90 && chemistry>= 90) || (math >= 90 && biology >=90) ||(chemistry >= 90 && biology>= 90 ))
+{
+    Console.WriteLine("Congratulations! You got accepted");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
---
+++ Week5LogicalOperator/Program.cs
@@ -0,0 +1,12 @@
+﻿// and -&&
+
+// true && true --> true
+// true && false --> false
+// False && true --> Flase
+// false && false --> false
+
+// Or -|| (pipes)
+// true || true --> true
+// true || false --> true
+// false || true --> true
+// false || false --> false
